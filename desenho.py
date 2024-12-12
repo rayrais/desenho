a04c8ce803a1706682e616e2ea8d7f0eccd9d89e
Ray30 <raiely.ferreira@escolar.ifrn.edu.br>
2024-12-12 09:35:21 -0300

    desenho.py
    desenho criativo de um quadrado

---
+++ desenho.py
@@ -0,0 +1,25 @@
+import turtle
+turtle.shape("turtle")
+turtle.color("blue")
+turtle.setup(800,600)
+turtle.title("tortuguita azulzinha")
+
+a= (100,30)
+b= (100,80)
+c= (34,80)
+d= (34,10)
+
+turtle.goto(0,0)
+turtle.down
+turtle.goto(100,0)
+turtle.down
+turtle.goto(100,100)
+turtle.down
+turtle.goto(0,100)
+turtle.down
+turtle.goto(0,0)
+turtle.down
+
+
+
+turtle.done()
